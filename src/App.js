780975a847eecc892c523fe52c2d03d432f7c447
Priyesh Bhanderi <priyesh.bhanderi@genixbit.com>
2025-03-23 11:37:47 +0530

    add

--- src/App.js
+++ src/App.js
@@ -6,24 +6,21 @@ function App() {
   const [more, setMore] = useState(false);
   const [share, setShare] = useState(false);
 
-  
+
   // Get the protocol, host, and port of the current page
   const protocol = window.location.protocol;  // 'http:' or 'https:'
   const host = window.location.hostname;      // e.g., '192.168.29.202'
   const port = window.location.port;          // Port, if available
-  
+
   // Dynamically update Open Graph meta tags
-  useEffect(() => {
-    // Clean the description to remove HTML tag
-    
-    // Update Open Graph meta tags in the head
+  const Updating = () => {
     document.querySelector('meta[property="og:title"]').setAttribute('content', 'title');
     document.querySelector('meta[property="og:description"]').setAttribute('content', 'हम यहाँ नवसारी के आयोजन मैं भी नारी शक्ति के सामर्थ्य को देख सकते हैं। : प्रधानमंत्री श्री');
     document.querySelector('meta[property="og:image"]').setAttribute('content', `https://img.youtube.com/vi/9WsSyatVk-c/0.jpg`);
     document.querySelector('meta[property="og:url"]').setAttribute('content', `${protocol}//${host}${port ? `:${port}` : ''}/?nid=81`);
     document.querySelector('meta[property="og:site_name"]').setAttribute('content', 'Info Gujrat');
-    document.title = 'title'; // Update the page title
-  }, []);
+    document.title = 'title';
+  }
 
   const shareUrl = `${protocol}//${host}${port ? `:${port}` : ''}/?nid=81`;
   const whatsappUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(shareUrl)}`;
@@ -33,11 +30,11 @@ function App() {
       <a
         target="_blank"
         href={whatsappUrl}
-        data-title={'title'}
-        data-description={'हम यहाँ नवसारी के आयोजन मैं भी नारी शक्ति के सामर्थ्य को देख सकते हैं। : प्रधानमंत्री श्री'}
+        data-title='title'
+        data-description='हम यहाँ नवसारी के आयोजन मैं भी नारी शक्ति के सामर्थ्य को देख सकते हैं। : प्रधानमंत्री श्री'
         data-image={`https://img.youtube.com/vi/9WsSyatVk-c/0.jpg`}
         data-url={whatsappUrl}
-        onClick={() => console.log('Updating OG Tags...')}
+        onClick={Updating}
         id="whatsapp-share"
         className="text-green-600"
       >
